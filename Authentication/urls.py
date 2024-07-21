028ba2280bcacb8f9e4c837fc762268718ac718e
dharun <dharunpalanisamy5256@gmail.com>
2024-07-21 12:17:04 +0530

    forgot password

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('userLogin/',UserLogin,name = 'UserLoginPage'),
     path(VERIFY_MAIL_ROUTE_STUDENT,VerifyStudent,name='verify student mail'),
     path(VERIFY_MAIL_ROUTE_COURSE_CENTER,VerifyCourseCenter,name='verify course center mail'),
-    path(CHANGE_PASSWORD,change_password,name='change password')
+    path(CHANGE_PASSWORD,change_password,name='change password'),
+    path('forgot_password_student',forget_password_student,name='forget password studetn'),
+    path('forgot_password_course_center',forget_password_course_center,name='forget_password_course_center')
 ]
