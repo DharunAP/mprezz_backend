313ef9b0c6a38203006ba97bbfbed7ac15c5fb8f
dharun <dharunpalanisamy5256@gmail.com>
2024-07-21 22:11:39 +0530

    crteate course

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list-all-courses/',ListAllCourses,name='course_listing'),
+    path('courses/',ListAllCourses,name='course_listing'),
     path('enrollCourse/',enrollCourse,name='enroll_course'),
     path('getCourse/<str:id>',getCourse,name='get-course')
 ]
